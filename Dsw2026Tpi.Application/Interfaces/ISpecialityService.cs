fd6504277de84e0c4b063b8ba763fd29d7430a59
valenbugeau <Valentina.Bugeau@alu.frt.utn.edu.ar>
2026-07-28 23:38:48 -0300

    Vale: Implementada baja lógica en Speciality, servicio completo y base de datos actualizada

---
+++ Dsw2026Tpi.Application/Interfaces/ISpecialityService.cs
@@ -0,0 +1,12 @@
+﻿using Dsw2026Tpi.Application.Dtos.Specialities;
+
+namespace Dsw2026Tpi.Application.Interfaces;
+
+public interface ISpecialityService
+{
+    Task<IEnumerable<SpecialityResponse>> GetAllAsync();
+    Task<SpecialityResponse> GetByIdAsync(Guid id);
+    Task<SpecialityResponse> CreateAsync(SpecialityRequest request);
+    Task UpdateAsync(Guid id, SpecialityRequest request);
+    Task DeleteAsync(Guid id); // Baja lógica
+}
