1c7b885ea963a975b881dce286ed5305ca3095f5
valenbugeau <Valentina.Bugeau@alu.frt.utn.edu.ar>
2026-08-02 01:41:50 -0300

    Vale: Interfaz IDoctorService finalizada con métodos CRUD y paginación

---
+++ Dsw2026Tpi.Application/Dtos/Doctors/DoctorResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Dsw2026Tpi.Application.Dtos.Doctors;
+
+public class DoctorResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string SpecialityName { get; set; } 
--- Dsw2026Tpi.Application/Interfaces/IDoctorService.cs
+++ Dsw2026Tpi.Application/Interfaces/IDoctorService.cs
@@ -1,9 +1,22 @@
-﻿using Dsw2026Tpi.Application.Dtos;
+﻿using Dsw2026Tpi.Application.Dtos.Doctors;
 using Dsw2026Tpi.Domain.Entities;
 
 namespace Dsw2026Tpi.Application.Interfaces;
 
 public interface IDoctorService
 {
-    Task<Pagination<DoctorModel.Response>> GetAll(int pageSize, int pageIndex, string? name = null);
+    
+    Task<Pagination<DoctorResponse>> GetAllAsync(int pageSize, int pageIndex, string? name = null);
+
+    // Obtener un médico por su ID
+    Task<DoctorResponse> GetByIdAsync(Guid id);
+
+    // Crear un médico nuevo (validando que la especialidad exista)
+    Task<DoctorResponse> CreateAsync(DoctorRequest request);
+
+    // Actualizar datos de un médico
+    Task UpdateAsync(Guid id, DoctorRequest request);
+
+   
+    Task DeleteAsync(Guid id);
 }
